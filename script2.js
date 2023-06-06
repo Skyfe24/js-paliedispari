344d8ba6458ac32ec0b73bc8ee2ede9c03e3c24d
Skyfe24 <fabio.erase@gmail.com>
2023-06-07 00:31:44 +0200

    EXCERCISE COMPLETED

--- script2.js
+++ script2.js
@@ -1,17 +1,31 @@
 console.log("JS OK");
-const min= 1;
-const max= 6;
-const random1 = Math.random();
-const random2 = Math.random();
+let utente = prompt('Scegli pari o dispari');
+let numero = parseInt(prompt('Inserisci un numero da 1 a 5:'));
 
-const randomUser = Math.floor(random1 * (max + 1 - min)) + min;
-console.log('il tuo risultato è ' + randomUser);
+//FUNZIONI
+function random(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-const randomCpu = Math.floor(random2 * (max + 1 - min)) + min;
-console.log('il risultato del CPU è ' + randomCpu);
+function pariODispari(pari_dispari) {
+  if(pari_dispari % 2 === 0){
+      return "pari";
+  }else{
+      return "dispari";
+  }
+}
 
-if (randomUser > randomCpu)
-  console.log('Questa volta sei stato fortunato, hai vinto');
-  else 
-  console.log('HAI PERSO MALE');
+let computer = random(1, 5);
+let sommaTot = computer + numero;
+let risultato = pariODispari(sommaTot);
+console.log('computer: ', computer);
+console.log('utente ', numero)
 
+
+if((risultato === 'pari' && utente === 'pari') || (utente === 'dispari' && risultato === 'dispari')){
+  document.getElementById("result_dice").innerHTML = ("non so se ti interessava della parola palindroma ma a dadi hai vinto &#128530;");
+    console.log("Hai vinto!");
+}else{
+    document.getElementById("result_dice").innerHTML = ("e comunque a dadi hai perso. &#128513;");
+    console.log("Hai perso =(");
+}
